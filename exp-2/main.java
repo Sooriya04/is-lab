3a7557a2495abe947c7e42604b57aa061b9a1cb8
Sooriya <158829004+Sooriya04@users.noreply.github.com>
2025-10-15 08:33:21 +0530

    Update main.java

--- exp-2/main.java
+++ exp-2/main.java
@@ -10,7 +10,8 @@ class HillCipher {
             { 2, 2, 19 }
     };
 
-    void encrypt(String ms) {
+    // Modified encrypt to RETURN the ciphertext instead of just printing
+    String encrypt(String ms) {
         pt = new int[3][3];
         ct = new char[3][3];
         int k = 0;
@@ -19,26 +20,24 @@ class HillCipher {
                 pt[j][i] = ms.charAt(k++) - 'a';
             }
         }
+        StringBuilder cipherText = new StringBuilder();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int z = 0; z < 3; z++) {
                     ct[i][j] += ((key[i][z] * pt[z][j]));
                 }
-
             }
-
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print((char) (ct[j][i] % 26 + 'a') + " ");
+                cipherText.append((char) (ct[j][i] % 26 + 'a'));
             }
         }
-
+        return cipherText.toString();
     }
 
     void decrypt(String ms) {
-        int[][] ik = new int[3][3];
-        ik = inverseKey(key);
+        int[][] ik = inverseKey(key);
 
         System.out.println();
         int p[][] = new int[3][3];
@@ -61,63 +60,20 @@ class HillCipher {
             }
         }
 
+        System.out.print("Decrypted: ");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print((char) (p[j][i] + 'a'));
             }
         }
-
     }
 
-    int determinant(int[][] mat) {
-        int x = (mat[0][0] * ((mat[1][1] * mat[2][2]) - (mat[2][1] * mat[1][2])))
-                - (mat[0][1] * ((mat[1][0] * mat[2][2]) - (mat[2][0] * mat[1][2])))
-                + (mat[0][2] * ((mat[1][0] * mat[2][1]) - (mat[2][0] * mat[1][1])));
-        return (x % 26 + 26) % 26;
+    // === Matrix functions remain unchanged ===
 
-    }
-
-    int detInv(int a, int b) {
-        BigInteger az = BigInteger.valueOf(a);
-        BigInteger bz = BigInteger.valueOf(b);
-        return (az.modInverse(bz)).intValue();
-    }
-
-    int[][] adjoint(int mat[][]) {
-        int res[][] = new int[3][3];
-        res[0][0] = (mat[1][1] * mat[2][2]) - (mat[2][1] * mat[1][2]);
-        res[0][1] = -((mat[1][0] * mat[2][2]) - (mat[2][0] * mat[1][2]));
-        res[0][2] = (mat[1][0] * mat[2][1]) - (mat[2][0] * mat[1][1]);
-
-        res[1][0] = -((mat[0][1] * mat[2][2]) - (mat[0][2] * mat[2][1]));
-        res[1][1] = (mat[0][0] * mat[2][2]) - (mat[2][0] * mat[0][2]);
-        res[1][2] = -((mat[0][0] * mat[2][1]) - (mat[2][0] * mat[0][1]));
-
-        res[2][0] = (mat[0][1] * mat[1][2]) - (mat[1][1] * mat[0][2]);
-        res[2][1] = -((mat[0][0] * mat[1][2]) - (mat[1][0] * mat[0][2]));
-        res[2][2] = (mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]);
-
-        int resT[][] = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                resT[i][j] = (res[j][i] % 26 + 26) % 26;
-            }
-        }
-        return resT;
-    }
-
-    int[][] inverseKey(int[][] matrix) {
-        int det = determinant(matrix);
-        int detInverse = detInv(det, 26);
-        int[][] adj = adjoint(matrix);
-        int[][] kInverse = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                kInverse[i][j] = (detInverse * adj[i][j]) % 26;
-            }
-        }
-        return kInverse;
-    }
+    int determinant(int[][] mat) { ... } // Keep same
+    int detInv(int a, int b) { ... }
+    int[][] adjoint(int mat[][]) { ... }
+    int[][] inverseKey(int[][] matrix) { ... }
 }
 
 public class main {
@@ -126,7 +82,10 @@ public class main {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the message\n");
         String msg = s.nextLine();
-        c.encrypt(msg);
-        c.decrypt("nlitjvrnd");
+
+        String enc = c.encrypt(msg);
+        System.out.println("Encrypted: " + enc);
+
+        c.decrypt(enc); // ✅ Now decrypting the real encrypted text
     }
 }
